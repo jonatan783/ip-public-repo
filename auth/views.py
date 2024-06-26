0fa2fb36935c15d940a99c372cddeb0fe44b90c6
Darky3rror <matiasadolfgs@gmail.com>
2024-06-25 21:30:59 -0300

    login

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    images, - []
+    return render(request, 'home.htlm', {'images': images})
+
+
+def getAllImagesAndFavouritelist(request):
+    images : []
+
+    return images
--- nasa_image_gallery/urls.py
+++ nasa_image_gallery/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index_page, name='index-page'),
     path('login/', views.index_page, name='login'),
+    
     path('home/', views.home, name='home'),
     path('buscar/', views.search, name='buscar'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 
     path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
     path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
